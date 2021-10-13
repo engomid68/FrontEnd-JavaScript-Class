79163b310f18e6a6cb5f6a327846cd36f41fa8bd
omid.asadi88@gmail.com <omid.asadi88@gmail.com>
2021-10-13 09:24:52 +0330

    edit slidMenu

--- src/js/slideMenu.js
+++ src/js/slideMenu.js
@@ -46,19 +46,19 @@ window.onload = function (e) {
     let i;
 
     for (i = 0; i < accordionMenu.length; i++) {
-            accordionMenu[i].addEventListener("click", function () {
-                for (let i = 0; i < accordionMenu.length; i++) {
-                    accordionMenu[i].classList.remove('active');
-                    accordionMenu[i].style.maxHeight = null;
-                    accordionMenu[i].nextElementSibling.style.maxHeight = null;
-                }
-                this.style.maxHeight = this.scrollHeight+'px';
-                this.classList.add('active');
-                var panel = this.nextElementSibling;
-                panel.style.maxHeight = panel.scrollHeight + 'px'
-                
-            });
-        }
+        accordionMenu[i].addEventListener("click", function () {
+            for (let i = 0; i < accordionMenu.length; i++) {
+                accordionMenu[i].classList.remove('active');
+                accordionMenu[i].style.maxHeight = null;
+                accordionMenu[i].nextElementSibling.style.maxHeight = null;
+            }
+            this.style.maxHeight = this.scrollHeight + 'px';
+            this.classList.add('active');
+            var panel = this.nextElementSibling;
+            panel.style.maxHeight = panel.scrollHeight + 'px'
+
+        });
+    }
 
 
     //***************************************  End Open Sub Menu On The SideBar **********************************/
@@ -68,9 +68,9 @@ window.onload = function (e) {
     let signupButton = document.querySelector(".signupButton");
     let closePopup = document.getElementById("closePopup");
 
-    signupButton.addEventListener('click' , showPopupModal);
-    closePopup.addEventListener('click' , closePopupModal);
-    overlay.addEventListener('click' , closePopupModal);
+    signupButton.addEventListener('click', showPopupModal);
+    closePopup.addEventListener('click', closePopupModal);
+    overlay.addEventListener('click', closePopupModal);
 
     function showPopupModal() {
         popupLogin.classList.add("active");
@@ -150,58 +150,58 @@ window.onload = function (e) {
     }
 
     //***************************************   End Header Main Menu Mobile **********************************/
-    
+
     let addItems = document.querySelector(".addItems");
     let modalMenuTablet = document.querySelector(".modalMenuTablet");
 
-    addItems.addEventListener('click' , funcShowMoreMenu);
+    addItems.addEventListener('click', funcShowMoreMenu);
 
     function funcShowMoreMenu() {
         modalMenuTablet.classList.toggle("active");
     }
-//***************************************   show subForms **********************************/
+    //***************************************   show subForms **********************************/
 
-//***************************************   End show subForms **********************************/
-dragElement(document.getElementById("draggable"));
+    //***************************************   End show subForms **********************************/
+    dragElement(document.getElementById("draggable"));
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "Header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementById(elmnt.id + "Header")) {
+            /* if present, the header is where you move the DIV from:*/
+            document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+        } else {
+            /* otherwise, move the DIV from anywhere inside the DIV:*/
+            elmnt.onmousedown = dragMouseDown;
+        }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
 
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+        function closeDragElement() {
+            /* stop moving when mouse button is released:*/
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
 }
